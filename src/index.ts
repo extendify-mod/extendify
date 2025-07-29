265a0fb98c068268dce8b7a543ab138e4cb9ce0c
elia <176603882+7elia@users.noreply.github.com>
2025-07-29 16:03:19 +0200

    update glob pattern

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import "@webpack/interceptor";
 import { loadEntrypoint } from "@webpack/loader";
 
 import.meta.glob(
-    ["./plugins/*/index.ts", "./plugins/*.ts", "./userplugins/*/index.ts", "./userplugins/*.ts"],
+    ["./{plugins,userplugins}/*/index.{ts,tsx}", "./{plugins,userplugins}/*.{ts,tsx}"],
     {
         eager: true
     }
