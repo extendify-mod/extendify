a751851581aa4cbb76000c239d0633347c6c1237
elia <176603882+7elia@users.noreply.github.com>
2025-10-07 19:59:44 +0200

    feat: add graphql utilities

---
+++ lib/api/gql.ts
@@ -0,0 +1,43 @@
+import { exportFilters, findAllModuleExports } from "@webpack/module";
+
+import { platform } from "./platform";
+
+interface Query {
+    name: string;
+    operation: "query";
+    sha256Hash: string;
+    value?: any;
+}
+
+interface QueryResult<T> {
+    data: T;
+}
+
+const queryCache: Record<string, Query> = {};
+
+export function findQuery(name: string): Query | undefined {
+    if (Object.keys(queryCache).includes(name)) {
+        return queryCache[name];
+    }
+
+    for (const query of findAllModuleExports<Query>(
+        exportFilters.byProps("name", "operation", "sha256Hash", "value")
+    )) {
+        if (query.operation !== "query") {
+            continue;
+        }
+
+        queryCache[query.name] = query;
+
+        if (query.name === name) {
+            return query;
+        }
+    }
+}
+
+export async function executeQuery<T>(
+    query: Query,
+    variables: Record<string, any>
+): Promise<QueryResult<T>> {
+    return (await platform?.getGraphQLLoader()(query, variables)) as QueryResult<T>;
+}
