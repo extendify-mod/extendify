7f7746a7cf85ce1d7d6e127c60a99798ab3331f5
elia <176603882+7elia@users.noreply.github.com>
2025-11-20 00:49:32 +0100

    feat: add quick actions to debug page

--- lib/components/settings/DebugPage.tsx
+++ lib/components/settings/DebugPage.tsx
@@ -1,10 +1,18 @@
 import "./debugPage.css";
 import "./extendifyPage.css";
 
+import { resolveApi } from "@api/platform";
+import { React } from "@api/react";
 import type { Icon } from "@components/icons";
+import { ButtonPrimary, Text } from "@components/spotify";
 import { exportFilters, findAllModuleExports } from "@webpack/module";
 
 import type { ComponentType } from "react";
+
+async function copyLogPath() {
+    const path: string = await (resolveApi("DesktopLogsAPI") as any).getLogFolder();
+    navigator.clipboard.writeText(path);
+}
 
 export default function () {
     const icons: ComponentType<Icon>[] = [
@@ -13,6 +21,18 @@ export default function () {
 
     return (
         <div className="ext-settings-section-layout">
+            <Text as="span" variant="titleSmall">
+                Actions
+            </Text>
+            <div>
+                <ButtonPrimary onClick={copyLogPath}>Copy Log Path</ButtonPrimary>
+                <ButtonPrimary onClick={() => navigator.clipboard.writeText(React.version)}>
+                    Copy React Ver.
+                </ButtonPrimary>
+            </div>
+            <Text as="span" variant="titleSmall">
+                Icons
+            </Text>
             <div className="ext-dbg-icons-container">
                 {icons.map((Icon) => (
                     <div className="ext-dbg-icon-container">
