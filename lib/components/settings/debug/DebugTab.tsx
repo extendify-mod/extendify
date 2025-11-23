17df64071489d6846bca1810a094ae70e4deb29a
elia <176603882+7elia@users.noreply.github.com>
2025-11-23 18:10:34 +0100

    feat: update debug tab

--- lib/components/settings/debug/DebugTab.tsx
+++ lib/components/settings/debug/DebugTab.tsx
@@ -1,12 +1,14 @@
 import "../extendifyPage.css";
 import "./debugTab.css";
+import "highlight.js/styles/github-dark.css";
 
 import { resolveApi } from "@api/platform";
-import { React } from "@api/react";
-import type { Icon } from "@components/icons";
-import { ButtonPrimary, Text } from "@components/spotify";
-import { exportFilters, findAllModuleExports } from "@webpack/module";
+import { React, useEffect, useState } from "@api/react";
+import { ArrowDownIcon, ArrowUpIcon, CopyIcon, type Icon } from "@components/icons";
+import { ButtonPrimary, ButtonTertiary, Text } from "@components/spotify";
+import { exportFilters, findAllModuleExports, findModuleExport } from "@webpack/module";
 
+import hljs from "highlight.js";
 import type { ComponentType } from "react";
 
 async function copyLogPath() {
@@ -15,9 +17,27 @@ async function copyLogPath() {
 }
 
 export default function () {
-    const icons: ComponentType<Icon>[] = [
-        ...Array.from(new Set(findAllModuleExports(exportFilters.byCode("svgContent:"))).values())
-    ];
+    const [icons, setIcons] = useState<ComponentType<Icon>[]>([]);
+    const [pkg, setPkg] = useState<string | undefined>();
+    const [pkgHidden, setPkgHidden] = useState(false);
+
+    useEffect(() => {
+        setIcons(
+            Array.from(new Set(findAllModuleExports(exportFilters.byCode("svgContent:"))).values())
+        );
+    }, []);
+
+    useEffect(() => {
+        findModuleExport<any>(exportFilters.byProps("name", "description", "version")).then(
+            (pkg) => {
+                setPkg(
+                    hljs.highlight(pkgHidden ? "{ ... }" : JSON.stringify(pkg, null, 4), {
+                        language: "json"
+                    }).value
+                );
+            }
+        );
+    }, [pkgHidden]);
 
     return (
         <div className="ext-settings-section-layout">
@@ -26,9 +46,35 @@ export default function () {
             </Text>
             <div>
                 <ButtonPrimary onClick={copyLogPath}>Copy Log Path</ButtonPrimary>
-                <ButtonPrimary onClick={() => navigator.clipboard.writeText(React.version)}>
-                    Copy React Ver.
-                </ButtonPrimary>
+            </div>
+            <Text as="span" variant="titleSmall">
+                Internal
+            </Text>
+            <div className="ext-dbg-codeblock">
+                {pkg ? (
+                    <>
+                        <div className="ext-dbg-codeblock-actions">
+                            <ButtonTertiary
+                                iconOnly={() => <CopyIcon />}
+                                onClick={() => navigator.clipboard.writeText(pkg)}
+                            />
+                            <ButtonTertiary
+                                iconOnly={() => (pkgHidden ? <ArrowUpIcon /> : <ArrowDownIcon />)}
+                                onClick={() => setPkgHidden(!pkgHidden)}
+                            />
+                        </div>
+                        <pre>
+                            <code
+                                className="hljs language-json"
+                                dangerouslySetInnerHTML={{ __html: pkg }}
+                            />
+                        </pre>
+                    </>
+                ) : (
+                    <Text as="span" variant="bodyMediumBold">
+                        Loading...
+                    </Text>
+                )}
             </div>
             <Text as="span" variant="titleSmall">
                 Icons
