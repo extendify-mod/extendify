d12d1e2146ec4650670ffa2f0c935cc9b3620125
elia <176603882+7elia@users.noreply.github.com>
2025-11-23 18:10:34 +0100

    feat: update debug tab

--- lib/components/icons.ts
+++ lib/components/icons.ts
@@ -7,7 +7,7 @@ export interface Icon {
 }
 
 function findIcon(code: string): LazyComponent<Icon> {
-    return findModuleComponent<Icon>(exportFilters.byCode(code));
+    return findModuleComponent(exportFilters.byCode(code));
 }
 
 export const InfoIcon = findIcon("M8 14.5a6.5");
@@ -15,3 +15,6 @@ export const EditIcon = findIcon("M11.838.714a2.438");
 export const CloseIcon = findIcon("M2.47 2.47a");
 export const GearIcon = findIcon("m15.51 7.484");
 export const GarbageIcon = findIcon("M5.25 3v-");
+export const CopyIcon = findIcon("M5 .75A.75.75");
+export const ArrowDownIcon = findIcon("M.47 4.97a");
+export const ArrowUpIcon = findIcon("M.47 11.03a");
