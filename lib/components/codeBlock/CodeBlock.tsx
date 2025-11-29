1764800786cec228589f72ef60dc8c5873579e2a
elia <176603882+7elia@users.noreply.github.com>
2025-11-30 00:21:56 +0100

    feat: separate CodeBlock component

---
+++ lib/components/codeBlock/CodeBlock.tsx
@@ -0,0 +1,52 @@
+import "./codeBlock.css";
+import "highlight.js/styles/github-dark.css";
+
+import { useEffect, useState } from "@api/react";
+import { ArrowDownIcon, ArrowUpIcon, CopyIcon } from "@components/icons";
+import { ButtonTertiary } from "@components/spotify";
+
+import hljs from "highlight.js";
+
+const EMPTY_CODE = "...";
+
+interface Props {
+    code?: string;
+    language: string;
+    hidden?: boolean;
+}
+
+export default function (props: Props) {
+    const [hidden, setHidden] = useState(props.hidden ?? !props.code);
+    const [code, setCode] = useState<string>(EMPTY_CODE);
+
+    useEffect(() => {
+        if (!props.code) {
+            return;
+        }
+
+        setCode(hljs.highlight(props.code, { language: props.language }).value);
+        setHidden(props.hidden ?? false);
+    }, [props.code]);
+
+    return (
+        <div className="ext-codeblock">
+            <div className="ext-codeblock-actions">
+                <ButtonTertiary
+                    iconOnly={() => <CopyIcon />}
+                    onClick={() => props.code && navigator.clipboard.writeText(props.code)}
+                    disabled={!props.code}
+                />
+                <ButtonTertiary
+                    iconOnly={() => (hidden ? <ArrowUpIcon /> : <ArrowDownIcon />)}
+                    onClick={() => setHidden(!hidden)}
+                />
+            </div>
+            <pre>
+                <code
+                    className={`hljs language-${props.language.toLowerCase().replaceAll(" ", "-")}`}
+                    dangerouslySetInnerHTML={{ __html: hidden ? EMPTY_CODE : (code ?? EMPTY_CODE) }}
+                />
+            </pre>
+        </div>
+    );
+}
--- lib/components/settings/debug/DebugTab.tsx
+++ lib/components/settings/debug/DebugTab.tsx
@@ -1,14 +1,13 @@
 import "../extendifyPage.css";
 import "./debugTab.css";
-import "highlight.js/styles/github-dark.css";
 
 import { resolveApi } from "@api/platform";
-import { React, useEffect, useState } from "@api/react";
-import { ArrowDownIcon, ArrowUpIcon, CopyIcon, type Icon } from "@components/icons";
-import { ButtonPrimary, ButtonTertiary, Text } from "@components/spotify";
+import { useEffect, useState } from "@api/react";
+import { CodeBlock } from "@components/codeBlock";
+import type { Icon } from "@components/icons";
+import { ButtonPrimary, Text } from "@components/spotify";
 import { exportFilters, findAllModuleExports, findModuleExport } from "@webpack/module";
 
-import hljs from "highlight.js";
 import type { ComponentType } from "react";
 
 async function copyLogPath() {
@@ -18,24 +17,15 @@ async function copyLogPath() {
 
 export default function () {
     const [icons, setIcons] = useState<ComponentType<Icon>[]>([]);
-    const [pkg, setPkg] = useState<string | undefined>();
-    const [pkgHidden, setPkgHidden] = useState(false);
+    const [pkg, setPkg] = useState<string>();
 
     useEffect(() => {
         setIcons(Array.from(new Set(findAllModuleExports(exportFilters.byCode("svgContent:")))));
-    }, []);
 
-    useEffect(() => {
-        findModuleExport<any>(exportFilters.byProps("name", "description", "version")).then(
-            (pkg) => {
-                setPkg(
-                    hljs.highlight(pkgHidden ? "{ ... }" : JSON.stringify(pkg, null, 4), {
-                        language: "json"
-                    }).value
-                );
-            }
-        );
-    }, [pkgHidden]);
+        findModuleExport(exportFilters.byProps("name", "description", "version")).then(pkg => {
+            setPkg(JSON.stringify(pkg, null, 4));
+        });
+    }, []);
 
     return (
         <div className="ext-settings-section-layout">
@@ -48,37 +38,12 @@ export default function () {
             <Text as="span" variant="titleSmall">
                 Internal
             </Text>
-            <div className="ext-dbg-codeblock">
-                {pkg ? (
-                    <>
-                        <div className="ext-dbg-codeblock-actions">
-                            <ButtonTertiary
-                                iconOnly={() => <CopyIcon />}
-                                onClick={() => navigator.clipboard.writeText(pkg)}
-                            />
-                            <ButtonTertiary
-                                iconOnly={() => (pkgHidden ? <ArrowUpIcon /> : <ArrowDownIcon />)}
-                                onClick={() => setPkgHidden(!pkgHidden)}
-                            />
-                        </div>
-                        <pre>
-                            <code
-                                className="hljs language-json"
-                                dangerouslySetInnerHTML={{ __html: pkg }}
-                            />
-                        </pre>
-                    </>
-                ) : (
-                    <Text as="span" variant="bodyMediumBold">
-                        Loading...
-                    </Text>
-                )}
-            </div>
+            <CodeBlock code={pkg} language="json" />
             <Text as="span" variant="titleSmall">
                 Icons
             </Text>
             <div className="ext-dbg-icons-container">
-                {icons.map((Icon) => (
+                {icons.map(Icon => (
                     <div className="ext-dbg-icon-container">
                         <Icon />
                     </div>
