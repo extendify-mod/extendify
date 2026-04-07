08f36c0ee0c5de33f36273d320f16fda753c974f
Davri <42148912+Davr1@users.noreply.github.com>
2026-04-07 09:57:58 +0200

    chore: update settingsapi import

--- lib/shared/types/spotify/index.d.ts
+++ lib/shared/types/spotify/index.d.ts
@@ -1,11 +1,10 @@
 import type { AdManagers } from "@shared/types/spotify/ads";
 import type { EsperantoService } from "@shared/types/spotify/esperanto";
 import type { AnyExperiment } from "@shared/types/spotify/experiments";
+import type { SettingsAPI } from "@shared/types/spotify/settings";
 import type { User } from "@shared/types/spotify/user";
 
 import type { History } from "history";
-
-import type { SettingsAPI } from "./settings";
 
 export interface Platform {
     container: "Desktop";
