12a6af7aecd36a23e54ac9ff8a5864a8fe13bd91
elia <176603882+7elia@users.noreply.github.com>
2026-05-08 19:36:47 +0200

    feat(debug): add a way to test components

--- packages/mod/lib/components/settings/debug/DebugTab.tsx
+++ packages/mod/lib/components/settings/debug/DebugTab.tsx
@@ -1,7 +1,9 @@
 import { resolveApi } from "@api/platform";
-import { useEffect, useState } from "@api/react";
+import { React, ReactDOM, useEffect, useState } from "@api/react";
+import { moduleCache } from "@api/registry";
 import type { Icon } from "@components/icons";
-import { ButtonPrimary, Text } from "@components/spotify";
+import { TextInput } from "@components/input";
+import { ButtonPrimary, ButtonSecondary, Text } from "@components/spotify";
 import { exportFilters, findAllModuleExports } from "@webpack/module";
 
 import type { ComponentType } from "react";
@@ -12,6 +14,10 @@ async function copyLogPath() {
 }
 
 export default function () {
+    const [moduleId, setModuleId] = useState<number>();
+    const [exportName, setExportName] = useState<string>();
+    const [fakeProps, setFakeProps] = useState<string>("{}");
+
     const [icons, setIcons] = useState<ComponentType<Icon>[]>([]);
 
     useEffect(() => {
@@ -26,6 +32,64 @@ export default function () {
             <div>
                 <ButtonPrimary onClick={copyLogPath}>Copy Log Path</ButtonPrimary>
             </div>
+
+            <Text as="span" variant="titleSmall">
+                Create Component
+            </Text>
+            <div>
+                <TextInput
+                    onChange={value => setModuleId(Number(value))}
+                    placeholder="Module ID"
+                    type="number"
+                    value={moduleId}
+                />
+                <TextInput
+                    onChange={value => setExportName(value)}
+                    placeholder="Export name"
+                    value={exportName}
+                />
+                <TextInput
+                    onChange={value => setFakeProps(value)}
+                    placeholder="Props"
+                    value={fakeProps}
+                />
+
+                <ButtonPrimary
+                    onClick={() => {
+                        if (!moduleId || !exportName || !fakeProps) {
+                            return;
+                        }
+
+                        const container = document.getElementById("create-element-result");
+                        if (!container) {
+                            return;
+                        }
+                        container.innerHTML = "";
+                        const root = ReactDOM.createRoot(container);
+
+                        const element = React.createElement(
+                            (moduleCache[moduleId] as any)?.exports[exportName] ??
+                                "Module not found",
+                            JSON.parse(fakeProps)
+                        );
+                        root.render(element);
+                    }}
+                >
+                    Create
+                </ButtonPrimary>
+                <ButtonSecondary
+                    onClick={() => {
+                        const container = document.getElementById("create-element-result");
+                        if (container) {
+                            container.innerHTML = "";
+                        }
+                    }}
+                >
+                    Remove
+                </ButtonSecondary>
+            </div>
+            <div id="create-element-result"></div>
+
             <Text as="span" variant="titleSmall">
                 Icons
             </Text>
