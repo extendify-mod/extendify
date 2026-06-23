f2bbfb4724552da3eeccd6e2b3e915749166a2a5
7elia <176603882+7elia@users.noreply.github.com>
2026-06-24 00:42:41 +0200

    feat(nix): start mac support

--- nix/spotify-extendify.nix
+++ nix/spotify-extendify.nix
@@ -5,34 +5,43 @@
   makeWrapper,
   self,
   lib,
-}:
-symlinkJoin {
-  name = "spotify-extendify";
-  inherit (spotify) version;
+}: let
+  exe =
+    if stdenv.isDarwin
+    then "Applications/Spotify.app/MacOS/Spotify"
+    else "bin/spotify";
+  preloadVar =
+    if stdenv.isDarwin
+    then "DYLD_INSERT_LIBRARIES"
+    else "LD_PRELOAD";
+in
+  symlinkJoin {
+    name = "spotify-extendify";
+    inherit (spotify) version;
 
-  paths = [
-    spotify
-    self.packages.${stdenv.hostPlatform.system}.extendify-native
-  ];
-
-  buildInputs = [ makeWrapper ];
-
-  postBuild = ''
-    wrapProgram $out/bin/spotify \
-      --set LD_PRELOAD $out/lib/libextendify_native.so
-  '';
-
-  meta = {
-    mainProgram = "spotify";
-    description = spotify.meta.description + "(patched with Extendify)";
-    homepage = "https://github.com/extendify-mod/extendify";
-    # wtf is the license
-    # license = lib.licenses.gpl3;
-    maintainers = [ lib.maintainers.fazzi ];
-    platforms = [
-      "x86_64-linux"
-      "x86_64-darwin"
-      "aarch64-darwin"
+    paths = [
+      spotify
+      self.packages.${stdenv.hostPlatform.system}.extendify-native
     ];
-  };
-}
+
+    buildInputs = [makeWrapper];
+
+    postBuild = ''
+      wrapProgram $out/${exe} \
+        --set ${preloadVar} $out/lib/libextendify_native.so
+    '';
+
+    meta = {
+      mainProgram = "spotify";
+      description = spotify.meta.description + "(patched with Extendify)";
+      homepage = "https://github.com/extendify-mod/extendify";
+      # wtf is the license
+      # license = lib.licenses.gpl3;
+      maintainers = [lib.maintainers.fazzi];
+      platforms = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+    };
+  }
